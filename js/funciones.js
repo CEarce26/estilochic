bc92e6f2c87d90031d07bee6808e5b6e3250a525
CEarce26 <132491579+CEarce26@users.noreply.github.com>
2023-05-03 12:06:22 -0600

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,68 @@
+$(document).ready(function()
+{
+
+	/*alert("hola")*/
+
+	$(".bot").hide();
+	$(".muchachas").show();
+
+	
+	$(".ninos").on("click",function()
+	{
+		$(".ninitos").show();
+		$(".muchachas").hide();
+		$(".muchachos").hide();
+		$(".belle").hide();
+	});
+	
+	$(".mujeres").on("click",function()
+	{
+		$(".muchachas").show();
+		$(".muchachos").hide();
+		$(".belle").hide();
+		$(".ninitos").hide();
+	});
+
+	$(".hombres").on("click",function()
+	{
+		$(".muchachos").show();
+		$(".muchachas").hide();
+		$(".belle").hide();
+		$(".ninitos").hide();
+	});
+
+	$(".belleza").on("click",function()
+	{
+		$(".belle").show();
+		$(".ninitos").hide();
+		$(".muchachos").hide();
+		$(".muchachas").hide();
+	});
+
+
+
+
+	$(".ahora").on("click",function()
+	{
+		var texto1 = $(".nombre").val()
+		var texto2 = $(".comentario").val()
+
+		alert(texto1 + " " + texto2)
+
+		$(".publi").html("de:"+texto1+"<br>"+"comentario:"+texto2)
+
+	});
+
+	  const signUpButton = document.getElementById("signUp");
+      const signInButton = document.getElementById("signIn");
+      const container = document.getElementById("container");
+
+      signUpButton.addEventListener("click", () => {
+      container.classList.add("right-panel-active");
+   });
+
+      signInButton.addEventListener("click", () => {
+      container.classList.remove("right-panel-active");
+   });
+
+});
